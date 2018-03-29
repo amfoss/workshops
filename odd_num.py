a370f054895d01216be63ad3073c4e808b7cf18a
Aniketh01 <anikethgireesh@gmail.com>
2018-03-29 23:50:23 +0530

    Added few TODO inside odd_num.py

--- odd_num.py
+++ odd_num.py
@@ -5,4 +5,8 @@ upper_limit = int(input("Enter the upper limit"))
 
 for i in range(lower_limit, upper_limit):
     if (i % 2 != 0):
+        # TODO: Make it user friendly, print odd numbers are: i
         print(i)
+
+#other TODO: 1) Print statements if there is no numbers to print.
+#            2) Test if user left the lower_limit or upper_limit empty.
