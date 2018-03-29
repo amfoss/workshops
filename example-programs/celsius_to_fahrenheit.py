5b72aa89fa2203bd84c368f1760afd909570b26e
Chirath R <chirath.02@gmail.com>
2018-03-29 22:09:23 +0530

    example programs

---
+++ example-programs/celsius_to_fahrenheit.py
@@ -0,0 +1,8 @@
+# program to convert Celsius to Fahrenheit
+
+temp_celsius = input("Temperature in Celsius: ")
+
+temp_fahrenheit = temp_celsius * 1.8 + 32
+
+print("Celsius: " + str(temp_fahrenheit))
+
